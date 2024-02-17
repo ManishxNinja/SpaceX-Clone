d729ff7bd4ebcc3a09a018af05a854836a6e087b
ManishxNinja <manishkaushik2243@gmail.com>
2024-02-17 11:07:02 +0530

    HamburgerButton

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+     
+     cout << "namaste duniya :-0" << endl;
+     int a, b;
+     
+     
+     cout << "enter a:" <<endl;
+     cin>> a ;
+     
+     
+     cout << "enter b :" <<endl;
+     cin>> b ;
+    
+    if (a>b){
+        cout << "a is greater" << endl;
+    }
+    else if (a<b){
+        cout << "b is greater"<< endl;
+    
+    }
+    else {
+        cout << "a=b" << endl;
+    }
+}
