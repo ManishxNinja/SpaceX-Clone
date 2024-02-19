230b28ceedb4e8b466f9af6db6a7f9a578e390c6
ManishxNinja <manishkaushik2243@gmail.com>
2024-02-19 16:02:58 +0530

    InnerPages

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const mobileMenuanimationn = document.getElementById('mobile-menu');
+const counters = document.querySelectorAll('.counter');
+document.addEventListener('scroll',scrollpage);
+
+let scrollStarted = false;
 
 btn.addEventListener('click', navToggle);
 
@@ -12,4 +16,46 @@ function navToggle() {
   mobileMenuanimationn.classList.toggle('mobile-animaton');
 
 
+}
+
+function scrollpage(){
+  const scrollpos = window.scrollY;
+
+  if(scrollpos > 100 && !scrollStarted){
+    countUp();
+    scrollStarted = true;
+  }
+  else if(scrollpos < 100 && scrollStarted){
+    reset();
+    scrollStarted = false;
+  }
+}
+
+// Assuming you have an array of elements with class "counter"
+
+
+function countUp() {
+  counters.forEach(counter => {
+    counter.innerText = '0';
+
+    function updateCounter() {
+      const target = +counter.getAttribute('data-target');
+      let c = +counter.innerText;
+
+      const increment = target / 100;
+
+      if (c < target) {
+        counter.innerText = `${Math.ceil(c + increment)}`;
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+    }
+
+    updateCounter();
+  });
+}
+
+function reset() {
+  counters.forEach((counter) => (counter.innerHTML = '0'));
 }
